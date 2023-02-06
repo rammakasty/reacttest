9c4edc2ae12f8038fa16e59e28de65065525a077
choi ha young <gkdudk15@naver.com>
2023-02-07 08:12:26 +0900

    feat: Styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-    return <div className="App"></div>;
-}
+const App = () => {
+    const style = {
+        padding: '100px',
+        display: 'flex',
+        gap: '12px',
+    };
+
+    const squareStyle = {
+        width: '100px',
+        height: '100px',
+        border: '1px solid green',
+        borderRadius: '10px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    };
+
+    return (
+        <div style={style}>
+            <div style={squareStyle}>감자</div>
+            <div style={squareStyle}>고구마</div>
+            <div style={squareStyle}>오이</div>
+            <div style={squareStyle}>가지</div>
+            <div style={squareStyle}>옥수수</div>
+        </div>
+    );
+};
 
 export default App;
