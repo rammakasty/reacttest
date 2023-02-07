2efea7fb230984a2642c751aa6ad93b386826fc3
choi ha young <gkdudk15@naver.com>
2023-02-07 12:37:22 +0900

    feat: TodoBoard import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from 'reacttest';
 import './App.css';
+import TodoBoard from './components/todeBoard';
 
 function App() {
+    const [inputValue, setInputValue] = useState('');
+    const [todoList, setTodoList] = useState([]);
+    const addItem = () => {
+        console.log('im hererere!', inputValue);
+        setTodoList([...todoList, inputValue]);
+    };
     return (
         <main>
             <input type="text" onChange={(e) => setInputText(e.target.value)} />
-            <button>추가</button>
+            <button onClick={addItem}>추가</button>
+
+            <TodoBoard />
         </main>
     );
 }
